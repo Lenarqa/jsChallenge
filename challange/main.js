08dc525a4c956c3ef1b29ed87073e5de95d2f960
Lenarqa <lenar.valiev211@gmail.com>
2020-01-09 18:18:13 +0700

    begin 1 challenge

---
+++ challange/main.js
@@ -0,0 +1,10 @@
+//alert("Hello from main.js");
+//console.log("hello console!");
+
+//Challenge 1 age in days
+//document.getElementById('flex-box-result').innerHTML = "Hello!";
+function ageInDays(){
+    var birthYear = prompt("What year were you born?");
+    var ageInDayss = (2020 - birthYear) * 365;
+    console.log(ageInDayss);
+}
