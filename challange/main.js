80014d03db5e519881687b54a0f709aa63347ccc
Lenarqa <lenar.valiev211@gmail.com>
2020-01-10 01:20:40 +0700

    ended second challenge

--- challange/main.js
+++ challange/main.js
@@ -6,5 +6,22 @@
 function ageInDays(){
     var birthYear = prompt("What year were you born?");
     var ageInDayss = (2020 - birthYear) * 365;
-    console.log(ageInDayss);
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode("You are " + ageInDayss + " days old.");
+    h1.setAttribute("id", "ageInDays");
+    h1.appendChild(textAnswer);
+    document.getElementById("flex-box-result").appendChild(h1);
+    //console.log(ageInDayss);
+}
+
+function resetAnswear(){
+    document.getElementById("ageInDays").remove();
+}
+
+function generateCat(){
+    var image = document.createElement('img');
+    var div = document.getElementById("flex-box-container-2");
+    image.src = "https://api.thecatapi.com/api/images/get?format=src&type=gif&size=small"
+    div.appendChild(image);
+
 }
